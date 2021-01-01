97b4061acfd039a09cf13d2465266ec81ae39894
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-01-01 14:28:02 +0200

    [589][Tree] N-Ary tree traversal iteratively with stack

--- tree/generic_tree/n-ary_tree_traversal_linear_with_stack.py
+++ tree/generic_tree/n-ary_tree_traversal_linear_with_stack.py
@@ -1,35 +1,54 @@
-# 589: N-ary Tree Preorder Traversal
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = childre
+"""
 
-def preorder(self, root):
-	if root is None: # Work with only current incomming node
-		return
+# [DESCRIPTION] N working solution with help
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
 
-	result = []
-	stack = []
-	stack.append(root) # Add to stack right away
+        if not root:
+            return []
 
-	while stack: # While we have stack
-		node = stack.pop() # Remove and return the last item
-		result.append(node.val) # Add to result list
-		for i in node.children[::-1]: # Get this nodes' children and reverse it, removing the root (-1)
-			stack.append(i) # Append each item of the reversed order
+        values = []
+        stack = [root]
 
-	return result
+        while stack:
+            current = stack.pop()
+            values.append(current.val)
+            if current.children:
+                stack = stack + current.children[::-1]
+
+        return values
 
 
-def preorder(self, root):
+# [DESCRIPTION] First not working solution.
+# Main issues:
+# 	The main problem is basic knowledge in Python3.
+# 	Lack of understanding the iterative and recursive approaches
+# 		Comment: Why do you use stack (initialize it) if you create
+#				 some recursive function. There is no goal to just store
+#				 stack in memory and use it with recursion.
+#	Result of the previous: interlinks emerged.
 
-	order = list()
-	if root is None:return order
-	stack1 = []
-	stack2 = []
-	stack1.append(root)
-	while stack1:
-		curr = stack1.pop()
-		order.append(curr.val)
-		for child in curr.children:
-			stack2.append(child)
-		while stack2:
-			stack1.append(stack2.pop())
+#         values = []
+#         if not root:
+#             return values
 
-	return order
+#         def trace(node):
+#             stack = []
+
+#             if not node.children:
+#                 values.append(node.val)
+#                 stack = stack[:-1]
+
+#             while node.children:
+#                 stack = stack + node.children[::-1]
+#                 trace(stack[len(stack) - 1])
+
+#         trace(root)
+
+#         return values
