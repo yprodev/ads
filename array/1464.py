b035abf5e4c53cf40a332a9620ce66003af4cfa1
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-04-06 08:38:27 +0300

    [v][Array][1464] Success

--- array/1464.py
+++ array/1464.py
@@ -133,3 +133,20 @@ class Solution:
                     
         return res
             
+
+
+# [UPDATE]: Copied code from the author: brianchiang_tw
+
+class Solution(object):
+    def maxProduct(self, nums):
+        first, second = 0, 0
+
+        for number in nums:
+            if number > first:
+                # update first largest and second largest
+                first, second = number, first
+            else:
+                # update second largest
+                second = max( number, second)
+
+        return (first - 1) * (second - 1)
