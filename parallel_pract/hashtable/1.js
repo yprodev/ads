c74bb59080601322c9eaf68ffdaebed2298d66a0
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-06 06:17:42 +0300

    [HashTable] Add resizing to the hash table

--- parallel_pract/hashtable/1.js
+++ parallel_pract/hashtable/1.js
@@ -10,8 +10,38 @@ function hastStringToInt(string, tableSize) {
 
 class HashTable {
 	table = new Array(3);
+	numItems = 0;
+
+	resize = () => {
+		const newTable = new Array(this.table.length * 2);
+
+		for (let i = 0; i < this.table.length; i++) {
+			if (this.table[i]) {
+				this.table[i].forEach(([key, value]) => {
+					const index = hastStringToInt(key, newTable.length);
+
+					if (newTable[index]) {
+						newTable[index].push([key, value]);
+					} else {
+						newTable[index] = [[key, value]];
+					}
+
+				});
+			}
+		}
+
+		this.table = newTable;
+	}
 
 	setItem = (key, value) => {
+		this.numItems++;
+
+		const loadFactor = this.numItems / this.table.length;
+
+		if (loadFactor > .8) {
+			this.resize();
+		}
+
 		const index = hastStringToInt(key, this.table.length);
 
 		if (this.table[index]) {
@@ -35,9 +65,11 @@ class HashTable {
 
 const myTable = new HashTable();
 myTable.setItem('firstName', 'bob');
+console.log(myTable.table.length);
 myTable.setItem('lastName', 'tim');
 myTable.setItem('age', 5);
 myTable.setItem('dob', '1/2/3');
+console.log(myTable.table.length);
 console.log(myTable.getItem('firstName'));
 console.log(myTable.getItem('lastName'));
 console.log(myTable.getItem('age'));
