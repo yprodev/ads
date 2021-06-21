24cb82b0af52697d613782d31a6ce582480a157f
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-21 14:29:34 +0300

    [String][539] Minimum time difference - Cyclic algorithm

--- string/539.py
+++ string/539.py
@@ -117,8 +117,42 @@ var findMinDifference = function(timePoints) {
 # ===================================================
 
 
-# [AUTHOR] ME
-# [DESCRIPTION] First working solution
-# Issues:
-#	
+# [AUTHOR] strawberrykiwi
+# [DESCRIPTION] Re-typed solution for memorization
+
+
+"""
+
+1. Convert each time to minutes. O(n).
+2. Sort the list of minutes. O(nlogn).
+3. Calculate the distance from p[i] to p[i+1] for all i except p[n-1] where n is the length of the times array. O(n).
+4. Calculate the distance of the final point to it's first CW element, which will cross 0. This is the point in which there would be a revolution loop. O(1).
+
+At each step between 3 and 4, minDist = min(current, new).
+
+"""
+
+def toSec(time):
+    time = time.split(':')
+    res = (60 * int(time[0]) + int(time[1]))
+
+    return res
+
+
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        for i, time in enumerate(timePoints):
+            timePoints[i] = toSec(time)
+
+        timePoints.sort()
+
+        res = sys.maxsize
+
+        # Calculate the closest CW distance of each element except last
+        for i in range(0, len(timePoints) - 1):
+            res = min(res, (timePoints[i + 1] - timePoints[i]))
+
+        # Calculate the final point
+        return min(res, 60 * 24 - timePoints[-1] + timePoints[0])
+
 
