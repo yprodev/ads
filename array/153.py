c79dba7703594764f45f0d11d31050ea35f630c9
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-21 18:32:26 +0300

    [Array][153] Find minimum item in rotated array - binary search

--- array/153.py
+++ array/153.py
@@ -195,3 +195,25 @@ class Solution:
         # Does not work for number with the step [13, 15, 17, 19]
         return min(nums[diff], nums[diff - 1])
 
+
+
+# ===================================================
+
+
+# [AUTHOR] water1111
+# [DESCRIPTION] Re-typed solution for memorization
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            middle = (left + right) // 2
+
+            if nums[middle] > nums[right]:
+                left = middle + 1
+            else:
+                right = middle
+
+        return nums[left]
+
