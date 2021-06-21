4344a7b2bc2293a305d95a63dfbf7fcf676c653e
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-21 17:43:45 +0300

    [Two Pointers][986] Interval list intersections

--- two-pointers/986.py
+++ two-pointers/986.py
@@ -158,6 +158,7 @@ var intervalIntersection = function(A, B) {
 # [DESCRIPTION] First NOT working solution
 
 
+
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         res = []
@@ -190,4 +191,32 @@ class Solution:
 
 
         return res
+
+
+
+# ===================================================
+
+# [AUTHOR] arkaung
+# [DESCRIPTION] Re-typed solution for memorizing
+
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i = 0
+        j = 0
+        result = []
+
+        while i < len(firstList) and j < len(secondList):
+            a_start, a_end = firstList[i]
+            b_start, b_end = secondList[j]
+
+            if a_start <= b_end and b_start <= a_end:
+                result.append([max(a_start, b_start), min(a_end, b_end)])
+
+            if a_end <= b_end:
+                i += 1
+            else:
+                j += 1
+
+        return result
 
