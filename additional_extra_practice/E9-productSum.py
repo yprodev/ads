39c25b8931ee8864b1129e573e80f3cfbcf3ae5c
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-22 21:16:10 +0300

    [EXTRA][Easy] Get product sum, even if the element is array of numbers

---
+++ additional_extra_practice/E9-productSum.py
@@ -0,0 +1,14 @@
+# O(n) time | O(d) space | d - depth
+def productSum(array, multiplier = 1):
+	sum = 0
+
+	for element in array:
+		if type(element) is list:
+			sum += productSum(element, multiplier + 1)
+		else:
+			sum += element
+
+	return sum * multiplier
+
+
+
