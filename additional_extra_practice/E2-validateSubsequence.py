2fb3492dc21bf50a384fba7cd2837105c285a416
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-06-22 20:52:16 +0300

    [EXTRA][Easy] Validate the subsequence

---
+++ additional_extra_practice/E2-validateSubsequence.py
@@ -0,0 +1,27 @@
+# O(n) time | O(1) space
+def validateSubsequence1(array, sequence):
+	arrIdx = 0
+	seqIdx = 0
+
+	while arrIdx < len(array) and seqIdx < len(sequence):
+		if array[arrIdx] == sequence[seqIdx]:
+			seqIdx += 1
+
+		arrIdx += 1
+
+	return seqIdx == len(sequence)
+
+
+# O(n) time | O(1) space
+def validateSubsequence2(array, sequence):
+	seqIdx = 0
+
+	for value in array:
+
+		if seqIdx == len(sequence):
+			break
+
+		if sequence[seqIdx] == value:
+			seqIdx += 1
+
+	return seqIdx == len(sequence)
