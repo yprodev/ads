413697dc7f28c0e92c295f5bd4de8b7b1d21f148
Jerry McGuaier <yprodanchuk@gmail.com>
2021-11-14 07:09:28 +0200

    [EXTRA] Find N-th Fibonacci number
    Solution: brute-force, memoized and iterative solutions
    
    Tags: fibonacci, recursive, iterative, memoization, dynamic programming

---
+++ additional_extra_practice/E8-nthFibonacciNumber.py
@@ -0,0 +1,35 @@
+# O(2^n) time | O(n) space
+def getNthFibBrute(n):
+	if n == 2:
+		return 1
+
+	if n == 1:
+		return 0
+
+	return getNthFibBrute(n - 1) + getNthFibBrute(n - 2)
+
+
+# O(n) time | O(n) space
+def getNthFibMemoized(n, memoize = {1: 0, 2: 1}):
+	if n in memoize:
+		return memoize[n]
+	else:
+		memoize[n] = getNthFibMemoized(n - 1, memoize) + getNthFibMemoized(n - 2, memoize)
+		return memoize[n]
+
+
+# O(n) time | O(1) space
+def getNthFibIterative(n):
+	# one edge case: if we try to find the number
+	# at the position 1
+
+	lastTwo = [0, 1]
+	counter = 3
+
+	while counter <= n:
+		nextFib = lastTwo[0] + lastTwo[1]
+		lastTwo[0] = lastTwo[1]
+		lastTwo[1] = nextFib
+		counter += 1
+
+	return lastTwo[1] if n > 1 else lastTwo[0]
