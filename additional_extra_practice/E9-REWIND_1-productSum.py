b0095902f9c8eed9cf18c9b5c91adfffd97023e3
Jerry McGuaier <yprodanchuk@gmail.com>
2022-07-27 19:11:57 +0300

    [EXTRA] Easy 9: Iterate through special array and do product sum

--- additional_extra_practice/E9-REWIND_1-productSum.py
+++ additional_extra_practice/E9-REWIND_1-productSum.py
@@ -1,8 +1,14 @@
+# Product Sum task is the task with the special array.
+# While iterating through it you need to multiply the
+# numbers in it. In case the new array occurs, we sum
+# up elements in it and multiply the result by level
+# the of depth relative to the main array.
+
+
 # [ 5, 7, [ 7, -1 ], 3, [ 6, [ -13, 8 ], 4 ] ]
 # O(n) time | O(d) space | n - each sub-array + element in it; d - depth
 def productSum(array, multiplier = 1):
-	sum = 0;
-
+	sum = 0
 	for element in array:
 		if type(element) is list:
 			sum += productSum(element, multiplier + 1)
