4e47ac0a7ae39c0f050f645edb0759c0fbcc6461
Jerry McGuaier <yprodanchuk@gmail.com>
2022-08-06 21:15:06 +0300

    [EXTRA] Medium 4: Check monotonic array

---
+++ additional_extra_practice/M4-Monotonic-Array.py
@@ -0,0 +1,43 @@
+# O(n) time | O(1) space
+def isMonotonic(array):
+	if len(array) <= 2:
+		return True
+
+	direction = array[1] - array[0]
+
+	for i in range(2, len(array)):
+		if direction  == 0:
+			direction = array[i] - array[i - 1]
+			continue
+
+		if breaksDirection(direction, array[i - 1], array[i])
+			return False
+
+	return True
+
+def breaksDirection(direction, previous, current):
+	difference = current - previous
+
+	if direction > 0:
+		return difference < 0
+
+	return difference > 0
+
+
+# =================================================================
+# O(n) time | O(1) space
+def isMonotonicSimpler(array):
+	isNonDecreasing = True
+	isNonIncreasing = True
+
+	for i in range(1, len(array)):
+
+		if array[i] < array[i - 1]:
+			isNonDecreasing = False
+
+		if array[i] > array[i - 1]:
+			isNonIncreasing = False
+
+	return isNonIncreasing or isNonDecreasing
+
+
