40a59085817b1dc252ba156ab6be2550a508ab4b
Jerry McGuaier <yprodanchuk@gmail.com>
2022-08-30 00:44:43 +0300

    [EXTRA] Medium 16: Kadane's Algorithm

---
+++ additional_extra_practice/M16-Kadanes-Algorithm.py
@@ -0,0 +1,10 @@
+# O(n) time | O(1) space
+def kadanesAlgorithm(array):
+	maxEndingHere = array[0]
+	maxSoFar = array[0]
+
+	for num in array[1:]:
+		maxEndingHere = max(num, maxEndingHere + num)
+		maxSoFar = max(maxEndingHere, maxSoFar)
+
+	return maxSoFar
